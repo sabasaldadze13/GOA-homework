8372c1326cdb2beb327891829b4f27fcb3aa7ce8
sabasaldadze <sabasaldadze00@gmail.com>
2024-07-14 02:50:48 -0700

    first commit

---
+++ level 3/homework/hw.py
@@ -0,0 +1,12 @@
+name='saba'
+lastname='saldadze'
+country='georgia'
+scholl='merab choxoneliDzis saavtoro skola'
+age=12
+city='qutaisi'
+street='buxaidze'
+hobby='dance'
+pet='dog'
+academy='GOA academy' 
+print('my name is '+' '+name+' '+'my lastname is'+' '+lastname+' ,'+' '+'I live in '+' '+country+','+' '+'I study'+' '+scholl+','+' I am '+' '+str(age)
+       +','+' '+'I live in'+' '+city+','+ ' '+'on'+' '+street+'.'+' '+'my hobby is'+' '+hobby+','+' '+'I have very big'+' '+pet+' '+'I study at '+' '+academy+' .')
---
+++ level 3/homework/hw1.py
@@ -0,0 +1,5 @@
+name='saba'
+surname='saldadze'
+age=12
+hobby='dance'
+print(name,surname,age,hobby)
