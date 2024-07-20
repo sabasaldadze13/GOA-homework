7f1c96f7394f119efa634e0cefe9a9bcf5a95862
sabasaldadze <sabasaldadze00@gmail.com>
2024-07-20 04:41:40 -0700

    commit

---
+++ level 5/homework/hw.py
@@ -0,0 +1,26 @@
+#2დავალება
+name='saba'
+surname='saldadze'
+age=12
+country='georgia'
+city='kutaisi'
+pet='dog'
+scholl='merab choxonelidzis saavtoro skola'
+academy='GOA'
+hobby='dance'
+street='buxaidze'
+village='vani'
+print('my name is '+' '+name+' '+'my lastname is'+' '+surname+' ,'+' '+'I live in '+' '+country+','+' '+'I study'+' '+scholl+','+' I am '+' '+str(age)
+       +','+' '+'I live in'+' '+city+','+ ' '+'on'+' '+street+'.'+' '+'my hobby is'+' '+hobby+','+' '+'I have very big'+' '+pet+' '+'I study at '+' '+academy+' .'+' '+'I am reasting in the village'+','+' '+village+' .')
+
+#3დავალება
+#print(hello) აკლია ბრჭყალები
+#print'door'აკლია ბრჩხილები
+#print(12+"car")არ შეგვიძლია რომ სტრინგს დავუმატოთ ინტეჯერი
+#print 7+8 აკლია ბრჩხილები
+#1number="desk"არ შეიძლება ცვლადი დაიწყოს რიცხვით
+#big city="Tbilisi"აკლია "_"
+#car=mercedes მერსედესს სჭირდება ბრჭყალები
+#fruit-"banana"ცვლადი უნდა იყოს გატოლებული ტავის მნიშვნელობას თოლობის გარდა სხვა ნიშნის გამოყენება არ შეიძლება
+#print("street"-4)არ შეიძლება რომ სტინგს გამოვაკლოთ ინტეჯერი
+#street$="megobroba"ცვლადში არ შეიძლება სიმბოლოების გამოყენება 
